b6f9156da4816ba053b64821096ba78b7de19e0a
JSins <nutzlosundso@gmail.com>
2020-09-06 22:32:03 +0200

    juhu klappt

--- javascript.js
+++ javascript.js
@@ -35,6 +35,10 @@ const revclick = new Howl({
     volume: 0.5
 })
 
+const spulstop = new Howl({
+    src: ['audio/spulstop.mp3']
+})
+
 
 
 
@@ -48,6 +52,45 @@ let reving = false;
 let forwarding = false;
 let revtimer;
 let fortimer;
+let timer;
+let timerintervall;
+let rotation1 = 0;
+let rotation2 = 0;
+
+
+timer = setInterval(function(){
+    if(song.seek() < song.duration())
+    {
+        setsize();
+    }
+
+    if(playing == true)
+    {
+        rotation1 = rotation1 + 5 + 0.03 * song.seek();
+        rotation2 = rotation2 + 10 - 0.03 * song.seek();
+        $('#turner1').css("transform", "rotate(" + rotation1 + "deg)");
+        $('#turner2').css("transform", "rotate(" + rotation2 + "deg)");
+    }
+
+    if(forwarding == true)
+    {
+        rotation1 = rotation1 + 25;
+        rotation2 = rotation2 + 25;
+        $('#turner1').css("transform", "rotate(" + rotation1 + "deg)");
+        $('#turner2').css("transform", "rotate(" + rotation2 + "deg)");
+    }
+
+    if(reving == true)
+    {
+        rotation1 = rotation1 - 25;
+        rotation2 = rotation2 - 25;
+        $('#turner1').css("transform", "rotate(" + rotation1 + "deg)");
+        $('#turner2').css("transform", "rotate(" + rotation2 + "deg)");
+    }
+
+    endchecker();
+},50);
+
 
 $('#play').click(function(){
 if(playing == false)
@@ -96,9 +139,9 @@ $('#rev').click(function(){
 
         revtimer = setInterval(function(){
             currentpos = song.seek();
-            song.seek(currentpos - 2);
+            song.seek(currentpos - 0.5);
             console.log(currentpos);
-        }, 500)
+        }, 50)
     } 
 })
 
@@ -117,11 +160,11 @@ $('#for').click(function(){
         forsound.play();
         revclick.play();
 
-        fortimer = setInterval(function(){    
+        fortimer = setInterval(function(){     
             currentpos = song.seek();
-            song.seek(currentpos + 2);
+            song.seek(currentpos + 0.5);
             console.log(currentpos);
-        }, 500)
+        }, 50)
     } 
 })
 
@@ -148,19 +191,37 @@ $(document).on("input", "#volslider", function () {
 
 
 
-song.once('load', function(){
-console.log(song.duration());
-});
-
-// setInterval(function(){
-// setsize();
-// },10);
+// song.once('load', function(){
+// console.log(song.duration());
+// });
 
 let turn1size;
+let turn2size;
 
 function setsize(){
-    turn1size = 1.7 - song.seek() * 0.0033;
-    console.log(turn1size);
-    $('#turn1').css("transform", "scale(" + turn1size + ");")
+    turn1size = 1.3 - song.seek() * (0.6 / song.duration());
+    $('#turn1').css("transform", "scale(" + turn1size + ")");
+
+    turn2size = 0.7 + song.seek() * (0.6 / song.duration());
+    $('#turn2').css("transform", "scale(" + turn2size + ")");
 }
+
+function endchecker(){
+    if(song.seek() >= song.duration())
+    {
+        stopcounters();
+        playing = false;
+
+        noise.stop();
+        song.pause();
+        stopsound.play();
+        song.seek(song.duration() - 2);
+    }
+}
+
+song.on('end', function(){
+    playing = false;
+    noise.stop();
+    stopsound.play();
+});
   
